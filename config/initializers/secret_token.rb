a9c0bca6edd78eb69f96ccc02cdfd170f33eefc6
Tushar S <tsax@github>
2013-01-05 03:06:25 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PecuniaNunc::Application.config.secret_token = '91e0fef7e0a24ce171c7017f33e8b75ea14b82e98d93e82e1ac49866104312a2a35432f303ecf4e26f8d723ca5302f395374879c8449b38e2e517329b42fcbb0'
