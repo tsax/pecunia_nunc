c4512a00d23d9632f7c5f50b9e11874dae99db4e
Tushar S <tsax@github>
2013-01-16 04:20:17 -0500

    Created subscribers controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 PecuniaNunc::Application.routes.draw do
+  root :to => 'subscribers#index'
   resources :subscribers
 
 
@@ -51,7 +52,6 @@ PecuniaNunc::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
