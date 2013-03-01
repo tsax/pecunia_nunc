5ca4cfa5e346759ab2a555425d716378e75d5097
Tushar S <tsaxena86@gmail.com>
2013-02-28 22:59:35 -0500

    Comment on :pages => 1 in SubscribersController

--- app/mailers/subscriber_mailer.rb
+++ app/mailers/subscriber_mailer.rb
@@ -1,7 +1,7 @@
 require 'kickstarter'
 
 class SubscriberMailer < ActionMailer::Base
-  default from: "tsaxena86@gmail.com"
+  default from: "pecunia-nunc@tusharsaxena.com"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
